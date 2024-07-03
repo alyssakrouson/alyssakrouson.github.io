3de436838a0f3f24787e4ef6117628a11607120c
Alyssa Krouson <a2krouson@gmail.com>
2024-07-02 21:08:55 -0400

    changed structure and added socials

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,34 @@
+//buttons
+document.addEventListener('DOMContentLoaded', function() {
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    if (page == 'index.html') {
+        buttonColor = document.querySelectorAll('.navbar a');
+        buttonColor[0].style.backgroundColor = 'white';
+        buttonColor[0].style.color = 'black';
+    }
+    else if (page == 'projects.html') {
+        buttonColor = document.querySelectorAll('.navbar a');
+        buttonColor[1].style.backgroundColor = 'white';
+        buttonColor[1].style.color = 'black';
+    }
+    else if (page == 'linguistics.html') {
+        buttonColor = document.querySelectorAll('.navbar a');
+        buttonColor[2].style.backgroundColor = 'white';
+        buttonColor[2].style.color = 'black';
+    }
+    else if (page == 'contact.html') {
+        buttonColor = document.querySelectorAll('.navbar a');
+        buttonColor[3].style.backgroundColor = 'white';
+        buttonColor[3].style.color = 'black';
+    }
+    
+});
 //console greeting
 var consoleMessage = "Welcome to my Website!";
 console.log(consoleMessage)
 
-//Opening greeting
-function getGreeting() {
-    var today = new Date();
-    var hourNow = today.getHours();
-    var greeting;
-
-    if (hourNow > 18) {
-    greeting = 'Good evening!';
-    }
-    else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-    }
-    else if (hourNow > 0) {
-    greeting = 'Good morning!';
-    }
-    else {
-    greeting = 'Welcome!';
-    }
-    return greeting;
-    console.log(greeting)
-}
-var elTime = document.getElementById('greeting');
-elTime.textContent = getGreeting();
 
 //alert
 function sendAlert() {
@@ -33,3 +37,4 @@ function sendAlert() {
 }
 var email = document.getElementById('email');
 email.onclick = sendAlert;
+
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,19 +1,37 @@
 //sticky side-bar
-let sidebar = document.getElementsByClassName("side-bar")[0];
-let sidebar_content = document.getElementsByClassName("content-wrapper")[0];
+// let sidebar = document.getElementsByClassName("side-bar")[0];
+// let sidebar_content = document.getElementsByClassName("content-wrapper")[0];
 
-window.onscroll = () => {
-    let scrollTop = window.scrollY;
-    let viewHeight = window.innerHeight;
-    let contentHeight = sidebar_content.getBoundingClientRect().height;
-    let sidebarTop = sidebar.getBoundingClientRect().top + window.pageYOffset;
+// window.onscroll = () => {
+//     let scrollTop = window.scrollY;
+//     let viewHeight = window.innerHeight;
+//     let contentHeight = sidebar_content.getBoundingClientRect().height;
+//     let sidebarTop = sidebar.getBoundingClientRect().top + window.pageYOffset;
 
-    if (scrollTop >= contentHeight - viewHeight + sidebarTop) {
-        sidebar_content.style.transform = `translateY(-${contentHeight - viewportHeight + sidebar}px)`;
-        sidebar_content.style.position = "fixed";
-    }
-    else {
-        sidebar_content.style.transform = "";
-        sidebar_content.style.position = "";
-    }
+//     if (scrollTop >= contentHeight - viewHeight + sidebarTop) {
+//         sidebar_content.style.transform = `translateY(-${contentHeight - viewportHeight + sidebar}px)`;
+//         sidebar_content.style.position = "fixed";
+//     }
+//     else {
+//         sidebar_content.style.transform = "";
+//         sidebar_content.style.position = "";
+//     }
+// }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementsByClassName("side-bar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
